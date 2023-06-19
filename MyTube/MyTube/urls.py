aa4f81c57453daebcd8593bc232671ee08e8f490
JaneDoe88888 <JaneDoe888@yandex.ru>
2023-06-19 13:54:49 +0300

    added model

--- MyTube/MyTube/urls.py
+++ MyTube/MyTube/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
@@ -7,3 +9,4 @@ urlpatterns = [
     path('app/', include(('app.urls', 'app'), namespace='app'))
 ]
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- MyTube/app/models.py
+++ MyTube/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
-# Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='image/')
+    file = models.FileField(upload_to='video/', validators=[FileExtensionValidator(allowed_extensions=['mp4'])])
+    created_add = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
