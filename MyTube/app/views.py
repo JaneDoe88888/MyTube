6ab487f247689c4fb87675f5ddf871fc4408e660
JaneDoe88888 <JaneDoe888@yandex.ru>
2023-06-21 12:58:59 +0300

    added video.html

--- MyTube/app/views.py
+++ MyTube/app/views.py
@@ -5,3 +5,7 @@ from . models import Video
 def home(request):
     videos = Video.objects.all()
     return render(request, 'home.html', {'videos': videos})
+
+def video(request, pk):
+    video_object = Video.objects.get(pk=pk)
+    return render(request, 'video.html', {'video': video_object})
